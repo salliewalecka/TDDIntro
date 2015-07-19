55043a4b50122ca3140aa8fd207d47868a4d0e6c
salliewalecka <sallie.walecka@gmail.com>
2015-07-19 12:17:19 -0700

    shouldReturnOneWhenNumerIsOne() done

--- src/main/java/com/thoughtworks/tddintro/factorial/Factorial.java
+++ src/main/java/com/thoughtworks/tddintro/factorial/Factorial.java
@@ -1,7 +1,8 @@
 package com.thoughtworks.tddintro.factorial;
 
 public class Factorial {
-    public Integer compute(int i) {
-        return -1;
+
+    public Integer compute(int i){
+       return 1;
     }
 }
